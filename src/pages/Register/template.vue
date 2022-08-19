4ad0d3a287bd7e9108495a38622b0717ccf43beb
deibo <deib0@foxmail.com>
2022-08-19 21:42:40 +0800

    完成注册页面的样式和布局

--- src/pages/Register/template.vue
+++ src/pages/Register/template.vue
@@ -1,6 +1,17 @@
 <template>
     <div id="register">
-        register
+        <ul class="form">
+            <li class="username">
+                <h3>用户名</h3>
+                <el-input v-model="username" placeholder="请输入用户名"></el-input>
+            </li>
+            <li class="password">
+                <h3>密码</h3>
+                <el-input placeholder="请输入密码" v-model="password" show-password></el-input>
+            </li>
+        </ul>
+        <el-button class="register-btn" @click="onLogin">立即注册</el-button>
+        <p class="login">已有账号？<router-link class="signup-link" to="/login">登录</router-link></p>
     </div>
 </template>
 
